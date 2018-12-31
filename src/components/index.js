5d740b5237037fde09698a871ecb8f6199a2a0ef
Lucas Wilson <lkwilson96@gmail.com>
2018-12-30 14:29:53 -1000

    publish

--- src/components/index.js
+++ src/components/index.js
@@ -34,7 +34,7 @@ class Index extends React.Component {
     return (
       <div className={classes.root}>
         <Typography variant="h4" gutterBottom>
-          Pure Tone v0.3.4
+          Pure Tone v0.3.5
         </Typography>
       </div>
     );
